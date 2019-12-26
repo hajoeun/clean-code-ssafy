267f9d39ff7c182c8d0294dde07ec84bd110a690
Joeun Ha <imjoeunha@gmail.com>
2019-12-26 14:58:35 +0900

    Create example code

---
+++ src/js/data.js
@@ -0,0 +1,63 @@
+const loans = [
+    {
+        id: 1,
+        name: '이지페이론',
+        organization: '하나은행',
+        limit: 3500000,
+        interest: {
+            min: 4.39,
+            max: 6.55
+        },
+        is_prime: true
+    }, {
+        id: 2,
+        name: '신용대출(직장인)',
+        organization: '유진저축은행',
+        limit: 100000000,
+        interest: {
+            min: 5.9,
+            max: 24
+        },
+        is_prime: false
+    }, {
+        id: 3,
+        name: 'SBI중금리바빌론',
+        organization: 'SBI저축은행',
+        limit: 100000000,
+        interest: {
+            min: 5.9,
+            max: 18.9
+        },
+        is_prime: false
+    }, {
+        id: 4,
+        name: '직장인 신용대출',
+        organization: 'NH농협캐피탈',
+        limit: 50000000,
+        interest: {
+            min: 6.9,
+            max: 21.9
+        },
+        is_prime: false
+    }, {
+        id: 5,
+        name: '직장인K 신용대출',
+        organization: '케이뱅크',
+        limit: 150000000,
+        interest: {
+            min: 3.32,
+            max: 7.72
+        },
+        is_prime: true
+    }, {
+        id: 6,
+        name: '비상금 마이너스통장',
+        organization: '케이뱅크',
+        limit: 3000000,
+        interest: {
+            min: 3.76,
+            max: 8.51
+        },
+        is_prime: true
+    }
+];
---
+++ src/js/v1/index.js
@@ -0,0 +1,55 @@
+const $ = s => document.querySelector(s);
+
+const rend = d => (
+$('#loan_list').innerHTML =
+d.reduce((h, l) => (`
+    ${h}
+    <li class="loan_item">
+        <div class="name_and_organization">
+            <strong>${l.name}</strong>
+            <span>${l.organization}</span>
+        </div>
+        <br/>
+        <div class="limit_and_interest">
+            <div class="limit">
+                한도 <span>${l.limit}</span>
+            </div>
+            <div class="interest">
+                금리 <span>${l.interest.min} ~ ${l.interest.max}</span>
+            </div>
+        </div>
+    </li>
+`), ''));
+
+rend(loans);
+
+const current = {
+    loans: loans,
+    sort_by: 'register'
+};
+
+const compare = {
+    register: (a, b) => a.id - b.id,
+    interest: (a, b) => a.interest.min - b.interest.min,
+    limit: (a, b) => b.limit - a.limit
+};
+
+// s는 셀렉터, en은 이벤트명, f는 리스너
+const evt = (s, en, f) => $(s).addEventListener(en, f);
+const evt1 = (s, f) => evt(s, 'click', f);
+const evt2 = (s, f) => evt(s, 'change', f);
+// el은 엘리먼트, cn은 클래스명
+const has_c = (el, cn) => el.classList.contains(cn);
+const toggle_c = (el, cn) => el.classList.toggle(cn);
+
+evt1('#is_prime', ({ currentTarget }) => (
+    rend(current.loans = has_c(currentTarget, 'all') ?
+        current.loans.filter(loan => loan.is_prime) :
+        loans.sort(compare[current.sort_by]))
+    && toggle_c(currentTarget, 'all')
+));
+
+evt2('#sort', ({ currentTarget }) => (
+    rend(current.loans = current.loans.sort(
+        compare[current.sort_by = currentTarget.value]))
+));
